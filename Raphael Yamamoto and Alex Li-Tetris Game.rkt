e8ebabbc21166fc25c93e0835a07c45510b21a2e
Raphael Yamamoto <raphaely@aceraschool.org>
2019-02-17 20:14:10 -0500

    yeeet

--- Raphael Yamamoto and Alex Li-Tetris Game.rkt
+++ Raphael Yamamoto and Alex Li-Tetris Game.rkt
@@ -10,7 +10,8 @@
 ;;World Struct
 ;; contains the matrix, the score, and the active tetra (the one that is falling)
 
-;
+(require 2htdp/universe)
+(require 2htdp/image)
 
 (define-struct world (matrix active-tetra score))
 
@@ -19,9 +20,37 @@
 
 (define WIDTH 10)
 (define HEIGHT 20)
-(define BLOCK-SIZE 50) ;; size of individual block in grid
+(define BLOCK-SIZE 40) ;; size of individual block in grid
+
+(define dimensions (make-posn (* WIDTH BLOCK-SIZE) (* HEIGHT BLOCK-SIZE)))
+
+(define (make-grid WIDTH HEIGHT BLOCK-SIZE)
+  ;;(repeat 1 11 (lambda (index) (add-line vertical? index BLOCK-SIZE image)))
+  WIDTH
+  )
 
 (define grid (make-grid WIDTH HEIGHT BLOCK-SIZE))
+
+;;repeat : Number Number Function -> Number
+;;  iterates based on number from (in-range start end)
+;;
+;;(repeat 0 10 (lambda (n) (* n 1))) -> 0 1 2 3 4 5 6 7 8 9
+
+(define (repeat start end f)
+  (cond
+    [(>= start (- end 1)) (f start)]
+    [else (begin (f start) (repeat (+ 1 start) end f))])
+  )
+
+;;create-grid-posns : Number Number -> listofposns
+;;  creates grid coord
+;;
+
+(define (create-grid-posns WIDTH HEIGHT )
+  (repeat 1 (+ HEIGHT 1) (lambda (i) (repeat 1 (+ WIDTH 1) (lambda (j) ()))))
+  )
+
+(define grid-posns (create-grid-posns WIDTH HEIGHT))
 
 ;; Matrix
 ;; The matrix is a 2D lists of list
@@ -37,10 +66,6 @@
 ;;recur: list of anything function -> list of anything
 ;;  applies function to each element of the list and returns a list
 ;;
-
-(define (make-grid WIDTH HEIGHT BLOCK-SIZE)
-  
-  )
 
 ;; recurfunc : number number size-> image
 
@@ -66,34 +91,33 @@
 ;; the center of the tetra in which it rotates, and the rotation mode;
 ;; if the rotation point is at a corner of the block or if it is 
 
-(struct tetra (color center center-corner? blocks))
+(define-struct tetra (color center center-corner? blocks))
 
-(define (O-tetra "green" (posn 1 -1) true
-                 (list (posn 0 0) (posn 1 0) (posn 0 -1) (posn -1 -1))))
+(define O-tetra (make-tetra "green" (make-posn 1 -1) true
+                 (list (make-posn 0 0) (make-posn 1 0) (make-posn 0 -1) (make-posn -1 -1))))
 ;; the o block, which will be green, its 
 
-(define (L-tetra "purple" (posn 1 -1) false
-                 (list (posn 0 -1) (posn 1 -1) (posn 2 -1) (posn 2 0))))
+(define L-tetra (make-tetra "purple" (make-posn 1 -1) false
+                 (list (make-posn 0 -1) (make-posn 1 -1) (make-posn 2 -1) (make-posn 2 0))))
 
-(define (J-tetra "cyan" (posn 1 -1) false
-                 (list (posn 0 -1) (posn 1 -1) (posn 2 -1) (posn 0 0))))
+(define J-tetra (make-tetra "cyan" (make-posn 1 -1) false
+                 (list (make-posn 0 -1) (make-posn 1 -1) (make-posn 2 -1) (make-posn 0 0))))
 
-(define (I-tetra "darkblue" (posn 2 -1) true
-                 (list (posn 0 0) (posn 1 0) (posn 2 0) (posn 3 0))))
+(define I-tetra (make-tetra "darkblue" (make-posn 2 -1) true
+                 (list (make-posn 0 0) (make-posn 1 0) (make-posn 2 0) (make-posn 3 0))))
 
-(define (T-tetra "orange" (posn 1 -1) false
-                 (list (posn 0 -1) (posn 1 -1) (posn 1 0) (posn 2 -1))))
+(define T-tetra (make-tetra "orange" (make-posn 1 -1) false
+                 (list (make-posn 0 -1) (make-posn 1 -1) (make-posn 1 0) (make-posn 2 -1))))
 
-(define (Z-tetra "pink" (posn 1 -1) false
-                 (list (posn 0 0) (posn 1 0) (posn 1 -1) (posn 2 -1))))
+(define Z-tetra (make-tetra "pink" (make-posn 1 -1) false
+                 (list (make-posn 0 0) (make-posn 1 0) (make-posn 1 -1) (make-posn 2 -1))))
 
-(define (S-tetra "red" (posn 1 -1) false
-                 (list (posn 0 -1) (posn 1 -1) (posn 1 0) (posn 2 0))))
+(define S-tetra (make-tetra "red" (make-posn 1 -1) false
+                 (list (make-posn 0 -1) (make-posn 1 -1) (make-posn 1 0) (make-posn 2 0))))
 
 ;; recur : num num num (num -> num) -> (num -> num)
 ;; takes in a start, end, func -> start and end are now f(start) and f(end)
 ;; 
-
 
 
 
@@ -107,13 +131,19 @@
 ;;  given worldstate, returns an image
 ;;
 
-#;(define (draw w)
-  (draw-tetra (world-active-tetra w) grid)
+(define (draw w)
+  (rectangle (posn-x dimensions) (posn-y dimensions) "solid" "white")
   )
+
+(define (key k w)
+  k
+  )
+
+(define (tick w) w)
 
 ;;falalalalala
 
-#;(big-bang (world 0 T-tetra 0)
+(big-bang 0
           [on-tick tick]
           [on-draw draw]
           [on-key key])
